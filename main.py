b6b518018228fbd5d370aa077afa495576905b6d
Patricia Cabrera <patricia_cabrera-palomera@mednax.com>
2023-10-05 09:13:56 -0400

    Add functionality to convert word to morse code

---
+++ main.py
@@ -0,0 +1,10 @@
+from morse_translation import translation_dict
+
+word = input("Enter the word you would like to translate to morse code: ").upper()
+
+word_in_morse = ""
+
+for letter in word:
+    letter_in_morse = translation_dict.get(letter)
+    word_in_morse = word_in_morse + " " + letter_in_morse
+print(f"Translation to morse code: {word_in_morse}")
