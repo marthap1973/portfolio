50af7cf7ccb3a9094e356fd54b6acf1435a62e7f
Patricia Cabrera <98432947+marthap1973@users.noreply.github.com>
2023-10-05 16:38:53 -0400

    Reorgnize projects in separate folders

--- exercise_1/main.py
+++ exercise_1/main.py
@@ -1,4 +1,4 @@
-from morse_translation import translation_dict
+from exercise_1.morse_translation import translation_dict
 
 word = input("Enter the word you would like to translate to morse code: ").upper()
 
@@ -7,4 +7,4 @@ word_in_morse = ""
 for letter in word:
     letter_in_morse = translation_dict.get(letter)
     word_in_morse = word_in_morse + " " + letter_in_morse
-print(f"Translation to morse code: {word_in_morse}
+print(f"Translation to morse code: {word_in_morse}")
